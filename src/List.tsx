d1dd0e8c7e1cb9385da888a9e940fe408ff52b65
rose-phillips <ms.rosephillips@gmail.com>
2023-12-14 17:32:30 +0000

    added spinner

--- src/List.tsx
+++ src/List.tsx
@@ -1,80 +1,123 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from "react";
+import { Wheel } from "react-custom-roulette";
+
 import { v4 as uuidv4 } from "uuid";
 
 interface Item {
-    id: string;
-    text: string;
-    include: boolean;
-    score: number;
+  id: string;
+  option: string;
+  include: boolean;
+  score: number;
 }
 
 function List() {
-    const [list, setList] = useState<Item[]>(() => {
-        const saved = localStorage.getItem("SpinnerApp.list");
-        const initialValue = JSON.parse(saved!);
-        return initialValue || '';
-    })
+  const tempList = [
+    {
+      option: "0",
+      style: { backgroundColor: "green", textColor: "black" },
+    },
+  ];
+  const [list, setList] = useState<Item[]>(() => {
+    const saved = localStorage.getItem("SpinnerApp.list");
+    const initialValue = JSON.parse(saved!);
+    return initialValue || "";
+  });
 
-    const itemNameRef = useRef<null | HTMLInputElement>(null)
+  const itemNameRef = useRef<null | HTMLInputElement>(null);
 
-    const handleAdd = (e: any) => {
-        e.preventDefault()
-        if (itemNameRef.current?.value === '') return;
-        const newItem = { id: uuidv4().substr(0, 5), text: itemNameRef.current?.value!, include: true, score: 0 }
-        const newList = [...list, newItem]
-        setList(newList)
-        e.target.reset()
-    }
+  const handleAdd = (e: any) => {
+    e.preventDefault();
+    if (itemNameRef.current?.value === "") return;
+    const newItem = {
+      id: uuidv4().substr(0, 5),
+      option: itemNameRef.current?.value!,
+      include: true,
+      score: 0,
+    };
+    const newList = [...list, newItem];
+    setList(newList);
+    e.target.reset();
+  };
 
-    const handleToggle = (id: string) => {
-        const newList = list?.map((item) => {
-            if (item.id === id) return { ...item, include: !item.include }
-            else return item;
-        })
-        setList(newList)
-    }
+  const handleToggle = (id: string) => {
+    const newList = list?.map((item) => {
+      if (item.id === id) return { ...item, include: !item.include };
+      else return item;
+    });
+    setList(newList);
+  };
 
-    const handleClearAll = () => {
-        setList([])
-    }
+  const handleClearAll = () => {
+    setList([]);
+  };
 
-    useEffect(() => {
-        localStorage.setItem('SpinnerApp.list', JSON.stringify(list))
-    }, [list])
+  useEffect(() => {
+    localStorage.setItem("SpinnerApp.list", JSON.stringify(list));
+  }, [list]);
 
+  const handleShuffle = () => {
+    let shuffledList = list
+      .map((item) => ({ item, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ item }) => item);
+    setList(shuffledList);
+  };
 
-    const handleShuffle = () => {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
 
-        let shuffledList = list
-            .map(item => ({ item, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ item }) => item)
-        setList(shuffledList)
-    }
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * list.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
 
-    return (
-        <div className='my-auto mx-3 p-3'>
-            <form onSubmit={handleAdd} className='mb-3'>
-                <input ref={itemNameRef} placeholder='...add another item' type="text" />
-                <button type='submit'>add</button>
-            </form>
-            <button onClick={handleShuffle} className='mb-3'>shuffle</button>
+  return (
+    <>
+      <div className="my-auto mx-3 p-3">
+        <form onSubmit={handleAdd} className="mb-3">
+          <input
+            ref={itemNameRef}
+            placeholder="...add another item"
+            type="text"
+          />
+          <button type="submit">add</button>
+        </form>
+        <button onClick={handleShuffle} className="mb-3">
+          shuffle
+        </button>
 
-            {list?.map((item) => (
-                <div key={item.text} className='mb-1'>
-                    <label key={item.id}>
-                        <input key={item.id}
-                            type="checkbox"
-                            checked={item.include}
-                            onChange={() => handleToggle(item.id)}
-                        />{" "}
-                        {item.text} - {item.score}
-                    </label>
-                </div>
-            ))}
-            <button onClick={handleClearAll} className='mt-3'>delete all</button>
-        </div>
-    )
+        {list &&
+          list?.map((item) => (
+            <div key={item.option} className="mb-1">
+              <label key={item.id}>
+                <input
+                  key={item.id}
+                  type="checkbox"
+                  checked={item.include}
+                  onChange={() => handleToggle(item.id)}
+                />{" "}
+                {item.option} - {item.score}
+              </label>
+            </div>
+          ))}
+        <button onClick={handleClearAll} className="mt-3">
+          delete all
+        </button>
+      </div>
+      <div>
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={list.length > 0 ? list : tempList}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <button onClick={handleSpinClick}>SPIN</button>
+      </div>
+    </>
+  );
 }
 
-export default List
+export default List;
--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -1,12 +1,31 @@
-import './styles.css'
+import React, { useState } from "react";
+import { Wheel } from "react-custom-roulette";
 
 const Spinner = () => {
-    return (
-        <>
+  const data = [{ option: "0" }, { option: "1" }, { option: "2" }];
 
-        </>
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
 
-    )
-}
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
 
-export default Spinner
+  return (
+    <div>
+      <Wheel
+        mustStartSpinning={mustSpin}
+        prizeNumber={prizeNumber}
+        data={data}
+        onStopSpinning={() => {
+          setMustSpin(false);
+        }}
+      />
+      <button onClick={handleSpinClick}>SPIN</button>
+    </div>
+  );
+};
+
+export default Spinner;
