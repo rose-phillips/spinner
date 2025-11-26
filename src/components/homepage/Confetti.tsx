467679f9f8a59e1534ba99466ff01ab56fddd4c5
Richard Davidson <rdavidson@ybs.co.uk>
2025-11-26 14:49:28 +0000

    Added a random hex generator to the confetti component which can be used instead of the static values. Not hooked up yet but it's added for later configuration

--- src/components/homepage/Confetti.tsx
+++ src/components/homepage/Confetti.tsx
@@ -1,10 +1,28 @@
 import ConfettiExplosion from "react-confetti-explosion";
 
 const Confetti = ({ isExploding }: { isExploding: boolean }) => {
+
+    /**
+     * Generates a set number of random hex values and returns each one as a collective array
+     *
+     * @param length - The total number of colours to generate
+     * @returns Returns an array of colours.
+     *
+     */
+    const randomHexColorCode = (length:number = 3) => {
+        let returnHexes = [];
+        for(let i=1;i<=length;i++){
+            let n = (Math.random() * 0xfffff * 1000000).toString(16);
+            returnHexes.push('#' + n.slice(0, 6))
+        }
+        return returnHexes;
+    };
+
   return (
     <div className="confetti">
       {isExploding && (
         <ConfettiExplosion
+          // colors={randomHexColorCode(8)}
           colors={[
             "#10002b",
             "#240046",
