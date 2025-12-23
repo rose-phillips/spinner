5745b781a1b9776c5cbdcd3f4dbeb5d169c55964
Richard Davidson <rdavidson@ybs.co.uk>
2025-12-23 10:58:04 +0000

    FINALLY refactored the victory sound element with generic comps.

---
+++ src/components/settings/PlayButton.tsx
@@ -0,0 +1,45 @@
+import React, {useRef, useState} from "react";
+import {PreferenceStore, usePreferenceStore} from "../stores/PreferenceStore";
+import {camelCase} from "../../common/helpers/strings";
+
+interface IPlaybuttonProps {
+    soundType: string,
+    currentSoundFromPref: string
+}
+
+export const PlayButton: React.FC<IPlaybuttonProps> = ({
+    soundType,
+    currentSoundFromPref
+}) => {
+    soundType = camelCase(soundType);
+
+    const soundChoice = usePreferenceStore((state: PreferenceStore) => currentSoundFromPref);
+    const soundRef = useRef<HTMLAudioElement>(null);
+
+
+    const [play, setPlay] = useState("▶");
+
+    const playSound = () => {
+        if (soundRef.current) {
+            if (soundRef.current.paused) {
+                soundRef.current.volume = 0.15;
+                soundRef.current.play();
+                setPlay("⏹");
+            } else {
+                soundRef.current.pause();
+                soundRef.current.currentTime = 0;
+                setPlay("▶");
+            }
+        }
+    };
+    return (
+        <>
+        <span className="play-button" onClick={playSound}>Play Sound</span>
+        <audio
+            id={"victorySound-choice"}
+            src={soundChoice}
+            ref={soundRef}
+        ></audio>
+        </>
+    );
+};
