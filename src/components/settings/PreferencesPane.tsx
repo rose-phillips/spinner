14d3ebc5a160aa28d00bb2a0489c716e69913ff5
ligurmatic <github@ligurmatic.uk>
2025-12-29 22:31:50 +0000

    Update PreferencesPane.tsx

--- src/components/settings/PreferencesPane.tsx
+++ src/components/settings/PreferencesPane.tsx
@@ -1,47 +1,62 @@
 import SelectComponent from "./SelectComponent";
 import ToggleComponent from "./ToggleComponent";
 
-import {camelCase} from "../../common/helpers/strings";
+import { camelCase } from "../../common/helpers/strings";
 
 import soundList from '../SoundFiles.json';
 
-import { PreferenceStore, usePreferenceStore, Preferences } from "../stores/PreferenceStore";
+import { PreferenceStore, usePreferenceStore, Preferences } from '../stores/PreferenceStore';
 
 
 const PreferencesPaneComponent = () => {
 
-    console.log('preference store is ', usePreferenceStore)
-
-    const victorySoundChoiceFromPreferences = usePreferenceStore((state: PreferenceStore) => (state.preferences.victorySound === undefined ? "/sounds/kirbys-victory-dance.mp3" : state.preferences.victorySound[0].value));
+  const victorySoundChoiceFromPreferences = usePreferenceStore((state: PreferenceStore) => (state.preferences.victorySound === undefined ? "/sounds/kirbys-victory-dance.mp3" : state.preferences.victorySound[0].value));
 
   const setPreferences = usePreferenceStore(
-      (state: PreferenceStore) => state.setPreferences
-    );
+    (state: PreferenceStore) => state.setPreferences
+  );
 
+  const modal = document.querySelector('dialog');
+  const openPreferencesModal = (e: any) => {
+    e.preventDefault();
+    modal && modal.showModal();
 
+  }
 
-  const handleSelectChange = (e:any) => {
+  const closePreferencesModal = (e: any) => {
+    // e.preventDefault();
+    if (e.target === modal) {
+      modal && modal.close();
+    }
+  }
+
+  const handleSelectChange = (e: any) => {
     e.preventDefault();
     // grab the value of the select
     const preference = e.target.value
-      // create a normalised keyname with the select field name
+    // create a normalised keyname with the select field name
     const keyName = camelCase(e.target.name)
-    setPreferences({[keyName]: [{value: preference}]});
+    setPreferences({ [keyName]: [{ value: preference }] });
   }
 
   return (
-      <div className="w-75 m-5 preferences-pane">
-        <div className="preferences-popup">
-          <SelectComponent
+    <>
+      <button onClick={e => openPreferencesModal(e)}>
+        Open Preferences
+      </button>
+      <dialog onClick={e => closePreferencesModal(e)} >
+          <div className="preferences-popup">
+            <SelectComponent
               inputName="Victory Sound"
               defaultValue="Please Select"
               options={soundList.victorySounds}
               handleSelectChange={handleSelectChange}
               preferenceChoice={victorySoundChoiceFromPreferences}
-          />
-          <ToggleComponent label="Spin Sound"/>
-        </div>
-    </div>
+            />
+            <ToggleComponent label="Spin Sound" />
+          </div>
+      </dialog>
+    </>
   );
 };
 
