d83d28b8e223781f8724f01664fe957d0f5d0b38
Richard Davidson <rdavidson@ybs.co.uk>
2026-02-09 11:21:37 +0000

    Added in timeouts to the sound json for victory sounds - this should tighten up the start times for both sounds and better match the spin wheel motion with the audio

--- src/components/homepage/Spinner.tsx
+++ src/components/homepage/Spinner.tsx
@@ -4,96 +4,98 @@ import type { Item } from "./Home";
 import {usePreferenceStore} from "../stores/PreferenceStore";
 
 const Spinner = ({
-  list,
-  setOpen,
-  winner,
-  setWinner,
-  setIsExploding,
-}: {
-  list: Item[];
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  winner: Item;
-  setWinner: React.Dispatch<React.SetStateAction<Item>>;
-  setIsExploding: React.Dispatch<React.SetStateAction<boolean>>;
+					 list,
+					 setOpen,
+					 winner,
+					 setWinner,
+					 setIsExploding,
+				 }: {
+	list: Item[];
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	winner: Item;
+	setWinner: React.Dispatch<React.SetStateAction<Item>>;
+	setIsExploding: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const [includeOnWheel, setIncludeOnWheel] = useState<Item[]>([]);
-  const [mustSpin, setMustSpin] = useState(false);
-  const [prizeNumber, setPrizeNumber] = useState(0);
-  //
-  const tempList = [
-    {
-      option: "add people",
-      style: { backgroundColor: "white", textColor: "black" },
-    },
-  ];
-  //
-  useEffect(() => {
-    setIncludeOnWheel(list.filter((item) => item.include === true));
-  }, [list]);
-  //
-  const handleSpinClick = () => {
-    setIsExploding(false);
-    const newPrizeNumber = Math.floor(Math.random() * includeOnWheel.length);
-    setPrizeNumber(newPrizeNumber);
-    setMustSpin(true);
-    setTimeout(() => {
-      playSound(spinnerSoundRef, spinnerAutoplay);
-    }, 10);
-  };
+	const [includeOnWheel, setIncludeOnWheel] = useState<Item[]>([]);
+	const [mustSpin, setMustSpin] = useState(false);
+	const [prizeNumber, setPrizeNumber] = useState(0);
+	//
+	const tempList = [
+		{
+			option: "add people",
+			style: { backgroundColor: "white", textColor: "black" },
+		},
+	];
+	//
+	useEffect(() => {
+		setIncludeOnWheel(list.filter((item) => item.include === true));
+	}, [list]);
+	//
 
-  const {victorySound, spinnerSound, spinnerAutoplay, victorySoundAutoplay} = usePreferenceStore(preferences => preferences);
+	const {victorySound, spinnerSound, spinnerAutoplay, victorySoundAutoplay} = usePreferenceStore(preferences => preferences);
 
-  const spinnerSoundRef = useRef<HTMLAudioElement>(null);
-  const victorySoundRef = useRef<HTMLAudioElement>(null);
+	const spinnerSoundRef = useRef<HTMLAudioElement>(null);
+	const victorySoundRef = useRef<HTMLAudioElement>(null);
 
-  const playSound = (sound: React.RefObject<HTMLAudioElement>, autoplay:boolean) => {
-    if (sound.current) {
-      if (autoplay) {
-          sound.current.volume = 0.45;
-          sound.current.play();
-      }
-    }
-  };
-  //
+	const playSound = (sound: React.RefObject<HTMLAudioElement>, autoplay:boolean) => {
+		if (sound.current) {
+			if (autoplay) {
+				sound.current.volume = 0.45;
+				sound.current.play();
+			}
+		}
+	};
 
-  return (
-    <div className="wheel-container mx-5">
-      <Wheel
-        mustStartSpinning={mustSpin}
-        prizeNumber={prizeNumber}
-        data={includeOnWheel.length > 0 ? includeOnWheel : tempList}
-        onStopSpinning={() => {
-          playSound(victorySoundRef,victorySoundAutoplay);
-          setWinner(includeOnWheel[prizeNumber]);
-          setMustSpin(false);
-          setOpen(true);
-          setIsExploding(true);
-        }}
-        backgroundColors={["lightgrey", "grey", "darkgrey"]}
-        outerBorderWidth={0}
-        innerBorderColor="black"
-        innerBorderWidth={50}
-        radiusLineColor="black"
-        radiusLineWidth={2}
-        fontFamily="Roboto Mono"
-        fontWeight={200}
-        spinDuration={0.36}
-      />
-      <button className="spin-button" onClick={handleSpinClick}>
-        SPIN
-      </button>
-      <audio
-          id={"victorySound-choice"}
-          src={victorySound}
-          ref={victorySoundRef}
-      ></audio>
-      <audio
-          id={"spinnerSound-choice"}
-          src={spinnerSound}
-          ref={spinnerSoundRef}
-      ></audio>
-    </div>
-  );
+	const handleSpinClick = () => {
+		setIsExploding(false);
+		const newPrizeNumber = Math.floor(Math.random() * includeOnWheel.length);
+		setPrizeNumber(newPrizeNumber);
+		setMustSpin(true);
+
+		const delayMs = spinnerSound?.timeout ?? 10;
+		setTimeout(() => {
+			playSound(spinnerSoundRef, spinnerAutoplay);
+		}, delayMs);
+	};
+
+	return (
+		<div className="wheel-container mx-5">
+			<Wheel
+				mustStartSpinning={mustSpin}
+				prizeNumber={prizeNumber}
+				data={includeOnWheel.length > 0 ? includeOnWheel : tempList}
+				onStopSpinning={() => {
+					playSound(victorySoundRef,victorySoundAutoplay);
+					setWinner(includeOnWheel[prizeNumber]);
+					setMustSpin(false);
+					setOpen(true);
+					setIsExploding(true);
+				}}
+				backgroundColors={["lightgrey", "grey", "darkgrey"]}
+				outerBorderWidth={0}
+				innerBorderColor="black"
+				innerBorderWidth={50}
+				radiusLineColor="black"
+				radiusLineWidth={2}
+				fontFamily="Roboto Mono"
+				fontWeight={200}
+				spinDuration={0.36}
+			/>
+			<button className="spin-button" onClick={handleSpinClick}>
+				SPIN
+			</button>
+			<audio
+				id={"victorySound-choice"}
+				src={victorySound}
+				ref={victorySoundRef}
+			></audio>
+			<audio
+				id={"spinnerSound-choice"}
+				src={spinnerSound?.value}
+				ref={spinnerSoundRef}
+			></audio>
+		</div>
+	);
 };
 
 export default Spinner;
--- src/components/settings/SelectComponent.tsx
+++ src/components/settings/SelectComponent.tsx
@@ -1,45 +1,52 @@
 import {PlayButton} from "./PlayButton";
 
 type Option = {
-  id: string,
-  name: string,
-  value: string,
+	id: string,
+	name: string,
+	value: string,
+	timeout?: number,
 }
 
 type SelectProps = {
-    defaultValue: string,
-    inputName: string,
-    options: Option[],
-    handleSelectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
-    preferenceChoice: string
+	defaultValue: string,
+	inputName: string,
+	options: Option[],
+	handleSelectChange: (option: Option) => void
+	preferenceChoice: string
 }
+
 const SelectComponent = (props: SelectProps) => {
+	const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedValue = event.target.value;
+		const selectedOption = props.options.find((o) => o.value === selectedValue);
+		if (selectedOption) props.handleSelectChange(selectedOption);
+	};
 
-  return (
-    <div>
-        <label htmlFor={props.inputName}>{props.inputName} - </label>
-      <select
-        defaultValue={props.preferenceChoice}
-        name={props.inputName}
-        id={props.inputName}
-        className="preferenceInput__select"
-        onChange={props.handleSelectChange}
-        >
-            {props.options.map((option) => {
-                return (
-                    <option
-                        key={option.value}
-                        value={option.value}
-                    >
-                    {option.name}
-                    </option>
-                );
-            })}
-        </select>
+	return (
+		<div>
+			<label htmlFor={props.inputName}>{props.inputName} - </label>
+			<select
+				defaultValue={props.preferenceChoice}
+				name={props.inputName}
+				id={props.inputName}
+				className="preferenceInput__select"
+				onChange={onSelectChange}
+			>
+				{props.options.map((option) => {
+					return (
+						<option
+							key={option.value}
+							value={option.value}
+						>
+							{option.name}
+						</option>
+					);
+				})}
+			</select>
 
-        <PlayButton soundType={props.inputName} currentSoundFromPref={props.preferenceChoice} />
-    </div>
-  );
+			<PlayButton soundType={props.inputName} currentSoundFromPref={props.preferenceChoice} />
+		</div>
+	);
 };
 
 export default SelectComponent;
